3a3c74b6ef178a10b3f7fd44282b9d7d4dca547b
Raihan Aulia S <raihanauliasuryadinata@students.amikom.ac.id>
2026-05-25 22:50:17 +0700

    Add files via upload

---
+++ travel-mockup/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+if (menuToggle && navMenu) {
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+}
